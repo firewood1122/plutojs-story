ab21ab976ed35ea677988751dd3ea1828f12f5b2
firewood1122 <firewood_1122@163.com>
2021-03-09 06:42:22 +0000

    feat:travis-ci auto build

---
+++ precache-manifest.6cfc13eba923f848740a5774599b9e57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fbcd5967da87c754cdf6e742bab26e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d01c3e14072e81ad0c28.bundle.js"
+  },
+  {
+    "url": "runtime~main.d01c3e14072e81ad0c28.bundle.js"
+  },
+  {
+    "revision": "57fbf2bdce0a9f277ec1",
+    "url": "static/css/main.b8b7aa70.chunk.css"
+  },
+  {
+    "url": "vendors~main.d01c3e14072e81ad0c28.bundle.js"
+  },
+  {
+    "url": "vendors~main.d01c3e14072e81ad0c28.bundle.js.LICENSE.txt"
+  }
+]);
